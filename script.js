5230d2e2bce047b0573c6fc2471237d12b4f6ab1
Matthew Slanaker <matt.slanaker@gmail.com>
2021-05-30 13:19:50 -0400

    added text boxes and for loop to change current hour to green

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log(moment());
+console.log("I loaded correctly")
+
+// creating an array like object
+var row = document.getElementsByClassName("custom-middle");
+
+var currentHour = parseInt(moment().format('H'));
+
+console.log(currentHour);
+
+checkHour()
+
+function checkHour() {
+    for (var i = 0; i < row.length; i++) {
+        var currentRow = row[i]
+        var rowID = currentRow.getAttribute("id")
+        var compareId = parseInt(rowID)
+        if (compareId === currentHour) {
+            currentRow.classList.add("green")
+        } else {
+            currentRow.classList.add("white")
+        }
+    } 
+}
